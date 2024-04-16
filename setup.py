a1810852c9406e16bf188ee88ce717e69c512d58
mangesh <mrbalpande21@gmail.com>
2024-04-16 14:37:37 +0530

    Requirements are updated

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'mangesh balpande',
+    author_email= 'mrbalpande21@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
