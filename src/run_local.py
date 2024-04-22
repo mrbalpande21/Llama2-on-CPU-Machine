feb1ea78f3d78118e1175c436b41a90a50f6c887
Mangesh Balpande <mrbalpande21@gmail.com>
2024-04-22 15:12:35 +0530

    prompt updated

--- src/run_local.py
+++ src/run_local.py
@@ -7,7 +7,7 @@ B_INST, E_INST = "[INST]", "[/INST]"
 B_SYS, E_SYS = "<<SYS>>\n", "\n<</SYS>>\n\n"
 
 
-instruction = "Convert the following text from English to french: \n\n {text}"
+instruction = "Convert the following text from English to hindi: \n\n {text}"
 
 SYSTEM_PROMPT = B_SYS + DEFAULT_SYSTEM_PROMPT + E_SYS
 template = B_INST + SYSTEM_PROMPT + instruction + E_INST
